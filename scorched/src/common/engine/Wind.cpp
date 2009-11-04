ac0ca8c8688e7f76bd18de7a51828d105e57e8e0
scara <scara@c9da2a28-54b3-47b3-b95f-5c2c1cae7f67>
2009-11-04 17:32:33 +0000

    Added missing source files

---
+++ scorched/src/common/engine/Wind.cpp
@@ -0,0 +1,146 @@
+////////////////////////////////////////////////////////////////////////////////
+//    Scorched3D (c) 2000-2009
+//
+//    This file is part of Scorched3D.
+//
+//    Scorched3D is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    Scorched3D is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Scorched3D; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+#include <engine/Simulator.h>
+#include <common/RandomGenerator.h>
+#include <common/OptionsScorched.h>
+#include <landscapemap/LandscapeMaps.h>
+
+Wind::Wind() : context_(0)
+{
+
+}
+
+Wind::~Wind()
+{
+
+}
+
+void Wind::newLevel()
+{
+	FileRandomGenerator &random =
+		context_->getSimulator().getRandomGenerator();
+
+	switch(context_->getOptionsGame().getWindForce())
+	{
+		case OptionsGame::WindRandom:
+			windSpeed_ = (
+				(random.getRandFixed() * fixed(true, 59000)).asInt()); // ie range 0->5
+			break;
+		case OptionsGame::Wind1:
+		case OptionsGame::Wind2:
+		case OptionsGame::Wind3:
+		case OptionsGame::Wind4:
+		case OptionsGame::Wind5:
+			windSpeed_ = (
+				fixed(int(context_->getOptionsGame().getWindForce()) - 1));
+			break;
+		case OptionsGame::WindBreezy:
+			windSpeed_ = (
+				(random.getRandFixed() * fixed(true, 29000)).asInt());// ie range 0->2);
+			break;
+		case OptionsGame::WindGale:
+			windSpeed_ = (
+				(random.getRandFixed() * fixed(true, 29000)).asInt() + 3); // ie range 3->5);
+			break;
+		case OptionsGame::WindNone:
+		default:
+			windSpeed_ = (0);
+			break;
+	}
+
+	if (windSpeed_ > 0)
+	{
+		fixed winAngle = random.getRandFixed() * 360;
+		windStartAngle_ = (winAngle);
+		windAngle_ = (winAngle);
+	}
+	else
+	{
+		windStartAngle_ = (0);
+		windAngle_ = (0);
+	}
+
+	updateDirection();
+	updateChangeTime();
+}
+
+void Wind::updateDirection()
+{
+	if (windSpeed_ > 0)
+	{
+		fixed windDirX = (windAngle_ / fixed(180) * fixed::XPI).sin();
+		fixed windDirY = (windAngle_ / fixed(180) * fixed::XPI).cos();
+		FixedVector windDir(windDirX, windDirY, 0);
+		windDirection_ = (windDir);
+	}
+	else
+	{
+		windDirection_ = FixedVector::getNullVector();
+	}
+}
+
+void Wind::updateChangeTime()
+{
+	if (windSpeed_ <= 0) return;
+	if (context_->getOptionsGame().getWindType().getValue() ==
+		OptionsGame::WindChangeNever) return;
+
+	FileRandomGenerator &random =
+		context_->getSimulator().getRandomGenerator();
+
+	switch (context_->getOptionsGame().getWindType().getValue()) 
+	{
+	case OptionsGame::WindChangeSomeTimes:
+		windChangeTime_ = random.getRandFixed() * 30 + 30;
+		break;
+	case OptionsGame::WindChangeFrequently:
+		windChangeTime_ = random.getRandFixed() * 15 + 15;
+		break;
+	case OptionsGame::WindChangeConstantly:
+		windChangeTime_ = random.getRandFixed() * 5 + 5;
+		break;
+	case OptionsGame::WindChangeAlways:
+		windChangeTime_ = random.getRandFixed() * 2;
+		break;
+	}
+}
+
+void Wind::simulate(fixed frameTime)
+{
+	if (windSpeed_ <= 0) return;
+	if (context_->getOptionsGame().getWindType().getValue() ==
+		OptionsGame::WindChangeNever) return;
+
+	windChangeTime_ -= frameTime;
+	if (windChangeTime_ < 0)
+	{
+		FileRandomGenerator &random =
+			context_->getSimulator().getRandomGenerator();
+
+		fixed winAngle = random.getRandFixed() * 180 - 90;
+		winAngle = fixed(1) - winAngle.cos();
+		winAngle *= 25;
+		windAngle_ = windStartAngle_ + winAngle;
+
+		updateDirection();
+		updateChangeTime();
+	}
+}
---
+++ scorched/src/common/engine/Wind.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+//    Scorched3D (c) 2000-2009
+//
+//    This file is part of Scorched3D.
+//
+//    Scorched3D is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    Scorched3D is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Scorched3D; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_Wind_H__86995B4A_478E_4CFE_BD4C_79128DE51904__INCLUDED_)
+#define AFX_Wind_H__86995B4A_478E_4CFE_BD4C_79128DE51904__INCLUDED_
+
+#include <common/FixedVector.h>
+#include <engine/ScorchedContext.h>
+
+class Wind
+{
+public:
+	Wind();
+	virtual ~Wind();
+
+	void setScorchedContext(ScorchedContext *context) { context_ = context; }
+	void newLevel();
+	void simulate(fixed frameTime);
+
+	fixed getWindAngle() { return windAngle_; }
+	fixed getWindSpeed() { return windSpeed_; }
+	FixedVector &getWindDirection() { return windDirection_; }
+	bool getWindOn() { return (windSpeed_ > fixed(0)); }
+
+protected:
+	ScorchedContext *context_;
+	fixed windAngle_, windStartAngle_, windSpeed_;
+	fixed windChangeTime_;
+	FixedVector windDirection_;
+
+	void updateDirection();
+	void updateChangeTime();
+};
+
+#endif // !defined(AFX_Wind_H__86995B4A_478E_4CFE_BD4C_79128DE51904__INCLUDED_)
