81ea53b72790fbfefebe5aab8e842914d61ca7bf
scara <scara@c9da2a28-54b3-47b3-b95f-5c2c1cae7f67>
2010-08-07 21:27:04 +0000

    Updated distribution for VC10 deployment

--- scorched/src/common/image/ImagePngFactory.cpp
+++ scorched/src/common/image/ImagePngFactory.cpp
@@ -143,7 +143,7 @@ Image ImagePngFactory::loadFromBuffer(NetBuffer &buffer, bool readalpha)
 	info_ptr = png_create_info_struct(png_ptr);
 	if (info_ptr == NULL)
 	{
-	  png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+	  png_destroy_read_struct(&png_ptr, (png_infopp) 0, (png_infopp) 0);
 	  return Image();
 	}
 
@@ -154,7 +154,7 @@ Image ImagePngFactory::loadFromBuffer(NetBuffer &buffer, bool readalpha)
 	if (setjmp(png_jmpbuf(png_ptr)))
 	{
 	  /* Free all of the memory associated with the png_ptr and info_ptr */
-	  png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+	  png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) 0);
 	  /* If we get here, we had a problem reading the file */
 	  return Image();
 	}
@@ -210,7 +210,7 @@ Image ImagePngFactory::loadFromBuffer(NetBuffer &buffer, bool readalpha)
 	}
 	else
 	{
-		png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) 0);
 
 		Logger::log(S3D::formatStringBuffer(
 			"Invalid PNG format.\n"
@@ -224,7 +224,7 @@ Image ImagePngFactory::loadFromBuffer(NetBuffer &buffer, bool readalpha)
 	// END NEW CODE
 
 	/* clean up after the read, and free any memory allocated - REQUIRED */
-	png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+	png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) 0);
 
 	/* that's it */
 	return result;
